23ca195fa84ce7591adfafdea5dbe4c9b708bb07
kavya <kavyasaravanan1919@gmail.com>
2025-07-10 22:37:14 +0530

    finaltest

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -1,5 +1,21 @@
-import { Link } from "react-router-dom"
+import { useForm } from "react-hook-form"
+import { Link,useNavigate } from "react-router-dom"
 const Signin = () => {
+    const form = useForm({
+    defaultValues:
+      {
+          username:"",
+          password:""
+      }
+    
+  });
+    const {register,handleSubmit,formState} = form;
+    const {errors} = formState;
+    const navigate = useNavigate()
+    const onSubmit = (data) =>{
+    console.log(data);
+    navigate('/');
+  }
   return (
     <>
     <section>
@@ -10,31 +26,29 @@ const Signin = () => {
                         <p className="text-xl md:text-2xl lg:text-3xl text-[#073345] font-light font-[Poppins]">WELCOME</p>
                         <p className="text-[17px] md:text-xl lg:text-2xl text-[#073345] font-light font-[Poppins]">Navigate Your Future at Sea !</p>
                     </div>
-                    
-                    <form action="" className="flex flex-col gap-4" id="form">
+                    <form onSubmit = {handleSubmit(onSubmit)} noValidate className="flex flex-col gap-4" id="form">
                         <div className="flex flex-col space-y-2">
                             <label htmlFor="id" className="text-base font-medium font-[Poppins]">Username</label>
-                            <input type="" id="id" className=" border-b-2   px-2" />
-                            <p id="iderror"></p>
+                            <input type="text" {...register("username",{required:{value:true,message:"Username is required"}})} id="username" className="border-b-2 px-2" />
+                            <p className="text-red-500">{errors?.username?.message}</p>
                         </div>
                         <div className="flex flex-col">
                             <label htmlFor="password" className="text-base font-medium font-[Poppins]">Password</label>
-                            <input type="password" id="password"
+                            <input type="password" {...register("password",{required:"password is required"})} id="password"
                                 className=" border-b-2 px-2" />
-                            <p id="passworderror"></p>
+                            <p className="text-red-500">{errors?.password?.message}</p>
                         </div>
                         <div className="flex items-center gap-2">
                             <input type="checkbox" id="rem" />
                             <label htmlFor="rem" className="text-base font-medium font-[Poppins]">Remember Me</label>
                         </div>
-                        <Link to="/LandingPage" className="flex justify-center items-center font-[Poppins] bg-[#20d6f0] rounded-sm text-[#073345] p-1">
+                        <div className="flex justify-center items-center font-[Poppins] bg-[#20d6f0] rounded-sm text-[#073345] p-1">
                         <button className="font-[Poppins] bg-[#20d6f0] rounded-sm text-[#073345] p-1">Sign In</button>
-                        </Link>
+                        </div>
                     </form>
                     <div className="flex items-center justify-start space-x-2">
                         <p>Don't have an account? </p>
                         <Link to="/SeafarerRegistration"><p className="font-[Poppins] underline text-blue-600 text-sm">Register</p></Link>
-                        
                     </div>
                 </div>
             </div>
